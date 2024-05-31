a7a80ca42639fc7e5c87dcd5b281885626eba1d2
Adebest <aadebesta@gmail.com>
2024-05-31 10:25:30 +0100

    now working perfectly

--- src/ContextApi.js
+++ src/ContextApi.js
@@ -96,41 +96,7 @@ const onLoadSearch = async(recipe)=>{
 
 }
 
-// useEffect(()=>{
-//   if (!search) {
-//     onLoadSearch("pizza")
-//     return ()=>  onLoadSearch("pizza")
-//   }
-//   handleSearch()
-  
-//   return ()=> handleSearch()
 
-// },[search])
-
-useEffect(()=>{
-const url = 'https://webknox-recipes.p.rapidapi.com/recipes/quickAnswer?q=How%20much%20vitamin%20c%20is%20in%202%20apples%3F';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'acaa964e06msheb33891d8fb14efp128fd2jsn280fae11bda3',
-		'X-RapidAPI-Host': 'webknox-recipes.p.rapidapi.com'
-	}
-};
-
-async function fetch() {
-  try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-}
-
-// fetch()
-
-// return ()=> fetch()
-},[])
 
 useEffect(()=>{
   if (search) {
